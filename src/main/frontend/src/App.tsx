5994f75ac61cb246c6f575c9413d329d8f5e4418
Changuen Oh <ocg0229@gmail.com>
2023-01-19 21:52:58 +0900

    init

---
+++ src/main/frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [test, setTest] = useState(""); // 연동 테스트용 state
+
+  useEffect(() => {
+    axios
+      .get("/api/test")
+      .then((response) => setTest(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  // fetch
+  /*
+  useEffect(() => {
+    fetch("/api/test")
+      .then((response) => response.text())
+      .then(setTest);
+  }, []);
+  */
+
+  return <div>프론트엔드 백엔드 연동 테스트 : {test}</div>;
+}
+
+export default App;
---
+++ src/main/java/com/hansung/hansungcommunity/HansungCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.hansung.hansungcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansungCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HansungCommunityApplication.class, args);
+	}
+
+}
