b0a4f321cae578b390cd8826a244fe7cf4795ff6
htkwon <a3001sd@gmail.com>
2023-02-09 12:16:50 +0900

    QnaArticle - user 리파지토리
    QueryDsl 설정

--- src/main/java/com/hansung/hansungcommunity/repository/UserRepository.java
+++ src/main/java/com/hansung/hansungcommunity/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.hansung.hansungcommunity.repository;
 
+import com.hansung.hansungcommunity.entity.QnaArticle;
 import com.hansung.hansungcommunity.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>,
+        QuerydslPredicateExecutor<QnaArticle> {
 }
