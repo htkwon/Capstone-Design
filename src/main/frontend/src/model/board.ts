325dd72b7375276b44834a25cb13f4f645cf6d62
Sauvi9non <xxxdmstj@gmail.com>
2023-02-09 13:51:37 +0900

    user, board 모델링 수정

--- src/main/frontend/src/model/board.ts
+++ src/main/frontend/src/model/board.ts
@@ -1,37 +1,70 @@
 //-------------INTERFACE-------------
 
 /*
-* 게시판 유형 (추후 더 추가될 수 있음)
-*/
+ * 게시판 유형 (추후 변경 될 수 있음)
+ */
 export enum BoardType {
-    free,
-    question,
-    recruit,
-    notice,
-    summary
+  free,
+  question,
+  recruit,
+  notice,
+  summary //todo(은서): 마이페이지 내 공부요약글 추후 따로 빠질 수 있음
 }
 
 export interface Board {
-   type: BoardType;
-   id: string;
-   title: String;
-   context: String;
-   nickname: String;
-   profileImg?: String;
-   studentId: String;
-   time: String;
-   bookmark: number;
-   comment: Array<Reply>
-   report: number;
-   //todo(은서): 게시판 성격따라서 다른 옵션 추가해야할듯합니다.
+  type: BoardType;
+  id: string;
+  title: String;
+  article: String;
+  code?: String; //Q&A 게시판에서 사용
+  imgUrl?: Array<String>;
+  nickname: String;
+  profileImg?: String;
+  studentId: String;
+  createdDate: String;
+  modifiedDate: String;
+  bookmarks: number;
+  replies: Array<Reply>;
+  reports: number;
+  condition?: String; //구인게시판 조건
+  points? : number; //Q&A 포인트
+  //todo(은서): 게시판 성격따라서 다른 옵션 추가되어 질 수 있음
 }
 
 export interface Reply {
-    id: String;
-    nickname: String;
-    context: String;
-    time: String;
-    report: number;
-    isChosen?: boolean;
-    isParent?: boolean;
+  id: String;
+  nickname: String;
+  article: String;
+  createdDate: String;
+  modifiedDate: String;
+  reports: number;
+  isChosen?: boolean;
+  isParent?: boolean;
+}
+//-------------INTERFACE-------------
+
+export enum Track {
+  mobile,
+  web,
+  bigData,
+  digitalContent,
+  none, //1학년은 트랙 선택 아직 안되었기에 none
+}
+export interface User {
+  studentId: String;
+  password: String;
+  name: String;
+  nickname: String;
+  profileImg?: String;
+  point: number;
+  track1: Track;
+  track2: Track;
+  showOff?: Array<String>;
+
+  reply: Array<Reply>;
+  board: Array<Board>;
+  bookmark: Array<Board>;
+
+  dailySummary: Array<Board>;
+  selfIntroduction: String;
 }
