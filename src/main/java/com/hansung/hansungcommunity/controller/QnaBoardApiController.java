a5aceb7cc7b09309d8681e75b29014fe05ce218b
htkwon <a3001sd@gmail.com>
2023-03-04 15:41:39 +0900

    FEAT : #27 - 이미지 처리 백엔드 코드
    1. 이미지 url 가져다주는 코드 
    2. 이미지 경로 설정을 위한 config 파일

--- src/main/java/com/hansung/hansungcommunity/controller/QnaBoardApiController.java
+++ src/main/java/com/hansung/hansungcommunity/controller/QnaBoardApiController.java
@@ -7,8 +7,10 @@ import com.hansung.hansungcommunity.dto.ImageDto;
 import com.hansung.hansungcommunity.dto.QnaBoardDto;
 import com.hansung.hansungcommunity.dto.QnaBoardResponseDto;
 import com.hansung.hansungcommunity.entity.QnaBoard;
+import com.hansung.hansungcommunity.entity.User;
 import com.hansung.hansungcommunity.service.ImageService;
 import com.hansung.hansungcommunity.service.QnaBoardService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +21,8 @@ import org.springframework.web.multipart.MultipartFile;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 
@@ -56,6 +56,8 @@ public class QnaBoardApiController {
      */
     @PostMapping("/qnaBoardsNoImage/{userId}")
     public ResponseEntity<QnaBoardDto> create(@RequestBody QnaBoardDto dto, @PathVariable Long userId){
+        System.out.println(dto.getPoint());
+
         QnaBoardDto articleDto = qnaBoardService.post(userId,dto);
         return ResponseEntity.status(HttpStatus.OK).body(articleDto);
 
@@ -99,7 +101,27 @@ public class QnaBoardApiController {
         return new ResponseEntity<Object>("Success",HttpStatus.OK);
     }
 
+    @PostMapping("/return/imageUrl")
+    public String create2(MultipartFile[] multipartFiles) throws IOException {
 
+            MultipartFile image = multipartFiles[0];
+
+            String name = (new Date().getTime()) + "" + (new Random().ints(1000, 9999).findAny().getAsInt());
+            String originalName = image.getOriginalFilename();
+            String path = "C:\\images";
+            String extension = originalName.substring(originalName.lastIndexOf(".") + 1);
+            if (!new File(path).exists()) {
+                try {
+                    new File(path).mkdir();
+                } catch (Exception e) {
+                    e.getStackTrace();
+                }
+            }
+            File save = new File(path, name + "." + extension);
+            image.transferTo(save);
+
+            return "/images/"+name+"."+extension;
+    }
 
 
 }
