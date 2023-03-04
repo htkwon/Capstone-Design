d7e464c3392c22663f4449601fc405171e4865bd
htkwon <a3001sd@gmail.com>
2023-03-04 15:49:43 +0900

    FEAT : #15 - 자유게시판 목록 조회 백엔드 코드

---
+++ src/main/java/com/hansung/hansungcommunity/dto/FreeBoardListDto.java
@@ -0,0 +1,38 @@
+package com.hansung.hansungcommunity.dto;
+
+import com.hansung.hansungcommunity.entity.FreeBoard;
+import com.hansung.hansungcommunity.entity.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+public class FreeBoardListDto {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    //private User user ;
+    private Long stuId;
+
+    //TODO : 이미지
+    private LocalDateTime createdDate;
+    private int bookmarks;
+    //TODO: 댓글
+    private int report;
+
+    public FreeBoardListDto(FreeBoard board) {
+        this.id = board.getId();
+        this.title = board.getTitle();
+        this.content = board.getContent();
+        //this.user = board.getUser();
+        this.stuId = board.getUser().getId();
+        this.createdDate = board.getCreatedAt();
+        this.bookmarks = board.getBookmarks();
+        this.report = board.getReports();
+    }
+}
