b78bf990fb8c0a7a1b1333936c7473fab4ff2f85
Chan-GN <ocg0229@gmail.com>
2023-03-07 16:47:34 +0900

    FIX : #38 - AuditingFields 수정 시간 필드 분리
    - 조회수 기능 추가로 발생한 수정 시간 필드의 변경 문제 해결
    - ModifiedEntity로 수정 시간 필드 분리 후 FreeBoard, QnaBoard Entity의 수정 로직에 적용

---
+++ src/main/java/com/hansung/hansungcommunity/entity/ModifiedEntity.java
@@ -0,0 +1,29 @@
+package com.hansung.hansungcommunity.entity;
+
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+/**
+ * 수정 시간(공통 필드)를 담고 있는 Entity
+ * 현재, FreeBoard, QnaBoard 에만 적용함
+ * 추후 댓글에도 활용 가능
+ */
+
+@MappedSuperclass
+@Getter
+public class ModifiedEntity extends AuditingFields {
+
+    // 수정 일자
+    @DateTimeFormat(iso= DateTimeFormat.ISO.DATE_TIME)
+    @Column
+    private LocalDateTime modifiedAt;
+
+    public void modified() {
+        this.modifiedAt = LocalDateTime.now();
+    }
+
+}
