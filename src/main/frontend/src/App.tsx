41acba0b809326884d65eaafba340241017c3d97
srp0114 <ksy010114@gmail.com>
2023-03-12 01:28:23 +0900

    FEAT : #53 - Q&A 상세보기 구현 - Q&A 게시판에서 해당 게시물 클릭 시, 출력될 UI 구현 - 상세보기 라우팅 처리 및 페이지 이동 구현 - 언어 이미지 처리 안 된 상태

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/pages/Home";
 import BoardWrite from "./components/pages/BoardWritePage";
 import BoardMain from "./components/pages/Board/Free/BoardMain";
-import EditToolbar from "./components/layout/EditorToolbar"
 import { freeBoard } from "./components/data/BoardData";
 import QnABoard from "./components/pages/Board/QnABoard";
+import QnADetail from "./components/pages/Board/QnADetails";
 import RecruitBoard from "./components/pages/Board/RecruitBoard";
 import MyPage from "./components/pages/MyPage";
 import Notice from "./components/pages/Notice";
@@ -21,10 +21,11 @@ function App() {
         <Route path="/free" element={<BoardMain />}> 
               {/*<Route path="/uuid" element={<ViewPosting posting={}/>} />*/}
         </Route>
-        <Route path="/questions/*" element={<QnABoard/>}/>
+        <Route path="/questions" element={<QnABoard/>}/>
+        <Route path="/questions/:id" element={<QnADetail/>}/>
         <Route path="/recruit/*" element={<RecruitBoard/>}/>
-        <Route path="mypage" element={<MyPage/>}/>
-        <Route path="notice" element={<Notice/>}/>
+        <Route path="/mypage" element={<MyPage/>}/>
+        <Route path="/notice" element={<Notice/>}/>
       </Routes>
     </Router>
     </>
