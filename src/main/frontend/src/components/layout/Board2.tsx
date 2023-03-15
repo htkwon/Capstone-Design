a6e82f5ccbe6951a875f1cf6b03047a56ad991c3
Eunseo Kwak <50993103+Sauvi9non@users.noreply.github.com>
2023-03-15 22:25:54 +0900

    Revert "Feature/#53 details"

--- src/main/frontend/src/components/layout/Board2.tsx
+++ src/main/frontend/src/components/layout/Board2.tsx
@@ -4,9 +4,10 @@ import Box from '@mui/material/Box';
 import { RxAvatar } from "react-icons/rx";
 import { BsBookmarkStar } from "react-icons/bs";
 import { TfiCommentAlt } from "react-icons/tfi";
-import Time from "../layout/Time";
 import "../style/Board.css";
 import axios from "axios";
+
+
 
 const Board2: React.FC = () => {
 
@@ -18,6 +19,32 @@ const Board2: React.FC = () => {
             .then((res) => setBoard2Items(res.data))
             .catch((err)=>console.log(err));
     },[])
+
+    const timeForToday = (value: string) => {
+        const today = new Date();
+        const timeValue = new Date(value);
+
+        const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+        if (betweenTime < 1) return '방금전';
+        if (betweenTime < 60) {
+            return `${betweenTime}분전`;
+        }
+
+        const betweenTimeHour = Math.floor(betweenTime / 60);
+        if (betweenTimeHour < 24) {
+            return `${betweenTimeHour}시간전`;
+        }
+
+        const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+        if (betweenTimeDay < 365) {
+            return `${betweenTimeDay}일전`;
+        }
+
+        return `${Math.floor(betweenTimeDay / 365)}년전`;
+    }
+
+
+
 
     return (
         <>
@@ -31,7 +58,7 @@ const Board2: React.FC = () => {
                     opacity: [1.0, 0.8, 0.7],
                     },}} className="box">
                     <p><RxAvatar size={30} className="icon"/></p> 
-                    <p className="name">{posting.nickname} · <Time date={posting.createdDate}/> 
+                    <p className="name">{posting.nickname} · {timeForToday(posting.createdDate)}
                     <img className="language" src={posting.language} /> </p>
                     <p className="title">{posting.title}</p>
                     <p className="comment">
