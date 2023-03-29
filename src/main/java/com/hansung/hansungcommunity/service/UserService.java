f8f6d8f2d251b8c5ca62aceb8f6c5b823190f19e
htkwon <a3001sd@gmail.com>
2023-03-29 16:43:03 +0900

    FEAT : #82 - 회원가입 처리 관련 로직

--- src/main/java/com/hansung/hansungcommunity/service/UserService.java
+++ src/main/java/com/hansung/hansungcommunity/service/UserService.java
@@ -1,10 +1,14 @@
 package com.hansung.hansungcommunity.service;
 
+import com.hansung.hansungcommunity.dto.UserRequestDto;
 import com.hansung.hansungcommunity.entity.User;
 import com.hansung.hansungcommunity.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor // 생성자 주입 (final 키워드)
@@ -16,9 +20,16 @@ public class UserService {
      * 회원가입 ( 학사 API 활용 시, 추가 정보 입력 용도 )
      */
     @Transactional // 필요 시 쓰기 전용
-    public Long join(User user) {
-        userRepository.save(user);
+    public Long join(UserRequestDto dto) {
+
+        User user = userRepository.save(User.of(dto));
 
         return user.getId();
+    }
+
+    public boolean checkUser(String stuId) {
+        boolean check = userRepository.existsUserByStudentId(stuId);
+
+        return check;
     }
 }
