149f9dda8e38c0d002287e67fc8fd69bce91dd86
Chan-GN <ocg0229@gmail.com>
2023-04-01 18:06:16 +0900

    FEAT : #90 - Q&A게시판 User 연동
    하단에 나열된 기능들에 User 연동
    
    - 메인 페이지 Q&A 게시글 조회
    - Q&A 게시글 목록 조회
    - Q&A 게시글 상세 조회
    - Q&A 게시글 작성

--- src/main/java/com/hansung/hansungcommunity/dto/QnaBoardResponseDto.java
+++ src/main/java/com/hansung/hansungcommunity/dto/QnaBoardResponseDto.java
@@ -9,18 +9,26 @@ import java.time.LocalDateTime;
 public class QnaBoardResponseDto {
 
     private Long id;
-    private String nickname;
-    private LocalDateTime createdDate;
     private String title;
-    private int bookmarks;
+    private String content;
+    private String writer;
+    private LocalDateTime createdDate;
+    private String language;
+    private int bookmark;
+    private int reply;
+    private int point;
 
 
     public QnaBoardResponseDto(QnaBoard board) {
-
-        this.nickname = board.getUser().getNickname();
-        this.createdDate = board.getCreatedAt();
+        this.id = board.getId();
         this.title = board.getTitle();
-        this.bookmarks = board.getBookmarks();
+        this.content = board.getContent();
+        this.writer = board.getUser().getNickname();
+        this.createdDate = board.getCreatedAt();
+        this.language = board.getLanguage();
+        this.bookmark = board.getBookmarks();
+        this.reply = board.getReplies().size();
+        this.point = board.getPoint();
     }
 
 }
