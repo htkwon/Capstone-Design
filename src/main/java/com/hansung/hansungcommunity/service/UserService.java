fb4668e6bc5e1f21c7f0f6ac3c1471fc00435d6d
htkwon <a3001sd@gmail.com>
2023-04-04 12:49:55 +0900

    FEAT : #101 - 메인페이지 유저 정보 받기
    -UserInfoDto  클래스 생성

--- src/main/java/com/hansung/hansungcommunity/service/UserService.java
+++ src/main/java/com/hansung/hansungcommunity/service/UserService.java
@@ -1,5 +1,6 @@
 package com.hansung.hansungcommunity.service;
 
+import com.hansung.hansungcommunity.dto.UserInfoDto;
 import com.hansung.hansungcommunity.dto.UserRequestDto;
 import com.hansung.hansungcommunity.entity.User;
 import com.hansung.hansungcommunity.repository.UserRepository;
@@ -41,5 +42,11 @@ public class UserService {
     public Optional<User> getByStudentId(String studentId) {
         return userRepository.findByStudentId(studentId);
     }
+
+    public UserInfoDto getUserInfo(Long stuId) {
+        User user = userRepository.findById(stuId).orElseThrow(()->new RuntimeException("유저가 존재하지 않습니다."));
+        return UserInfoDto.from(user);
+    }
+
 
 }
