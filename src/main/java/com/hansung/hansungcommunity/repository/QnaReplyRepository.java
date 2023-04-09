57db4690f4f7be353a9bebc2cdb5fee364c7d45b
htkwon <a3001sd@gmail.com>
2023-04-10 01:29:19 +0900

    FEAT : #105 - Q&A 게시판 채택 로직
    - 현재 Q&A 댓글 쪽  프론트단이 없어서 로직만 구현하였습니다. (테스트완료)

--- src/main/java/com/hansung/hansungcommunity/repository/QnaReplyRepository.java
+++ src/main/java/com/hansung/hansungcommunity/repository/QnaReplyRepository.java
@@ -1,9 +1,17 @@
 package com.hansung.hansungcommunity.repository;
 
+import com.hansung.hansungcommunity.entity.FreeReply;
 import com.hansung.hansungcommunity.entity.QnaReply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface QnaReplyRepository extends JpaRepository<QnaReply, Long>,
         QuerydslPredicateExecutor<QnaReply> {
+
+    @Query("SELECT f FROM QnaReply f WHERE f.board.id = :boardId")
+    List<QnaReply> findAllByBoardId(@Param("boardId") Long boardId);
 }
