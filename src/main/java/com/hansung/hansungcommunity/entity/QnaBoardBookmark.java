10cf5d6718fce6cb18c429a05527a168514db0dd
htkwon <a3001sd@gmail.com>
2023-04-11 16:07:00 +0900

    FEAT : QnaBookmark 엔티티 업로드

---
+++ src/main/java/com/hansung/hansungcommunity/entity/QnaBoardBookmark.java
@@ -0,0 +1,40 @@
+package com.hansung.hansungcommunity.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class QnaBoardBookmark {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name ="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name ="free_board_id")
+    private QnaBoard qnaBoard;
+
+    public QnaBoardBookmark(User user, QnaBoard qnaBoard) {
+        this.user = user;
+        this.qnaBoard = qnaBoard;
+    }
+
+    public static QnaBoardBookmark of(User user, QnaBoard qnaBoard){
+        return new QnaBoardBookmark(
+                user,
+                qnaBoard
+        );
+    }
+
+
+
+}
