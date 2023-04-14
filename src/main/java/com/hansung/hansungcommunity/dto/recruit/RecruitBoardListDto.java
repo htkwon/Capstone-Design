1af1477fd15499345def452a9619c6220a4c54ec
Chan-GN <ocg0229@gmail.com>
2023-04-14 17:13:25 +0900

    FEAT : #111 - 구인 게시판 목록, 상세 조회 연동
    - 목록, 상세 조회 관련 DTO 생성
    - 목록, 상세 조회 API 구현
    - 목록, 상세 조회 비즈니스 로직 구현
    - 상세 조회 시, 조회수 기능 구현
    - 구인 게시글 더미 데이터 생성을 위해 data.sql 수정

---
+++ src/main/java/com/hansung/hansungcommunity/dto/recruit/RecruitBoardListDto.java
@@ -0,0 +1,48 @@
+package com.hansung.hansungcommunity.dto.recruit;
+
+import com.hansung.hansungcommunity.entity.RecruitBoard;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RecruitBoardListDto {
+
+    private Long id;
+    private String title;
+    private String writer;
+    // private String profileImg;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+    private int bookmark;
+    // private int reply;
+    private int views;
+    private Long stuId;
+    // private List<String> imgUrl = new ArrayList<>();
+    private String require;
+    private String optional;
+    private int party;
+    private int gathered;
+
+    public static RecruitBoardListDto from(RecruitBoard recruitBoard) {
+        return new RecruitBoardListDto(
+                recruitBoard.getId(),
+                recruitBoard.getTitle(),
+                recruitBoard.getUser().getNickname(),
+                recruitBoard.getCreatedAt(),
+                recruitBoard.getModifiedAt(),
+                recruitBoard.getBookmarks(),
+                recruitBoard.getHits(),
+                Long.parseLong(recruitBoard.getUser().getStudentId()),
+                recruitBoard.getRequired(),
+                recruitBoard.getOptional(),
+                recruitBoard.getParty(),
+                recruitBoard.getGathered()
+        );
+    }
+
+}
--- src/main/java/com/hansung/hansungcommunity/service/RecruitBoardService.java
+++ src/main/java/com/hansung/hansungcommunity/service/RecruitBoardService.java
@@ -1,13 +1,21 @@
 package com.hansung.hansungcommunity.service;
 
+import com.hansung.hansungcommunity.dto.recruit.RecruitBoardDetailDto;
+import com.hansung.hansungcommunity.dto.recruit.RecruitBoardListDto;
 import com.hansung.hansungcommunity.dto.recruit.RecruitBoardRequestDto;
 import com.hansung.hansungcommunity.entity.RecruitBoard;
 import com.hansung.hansungcommunity.entity.User;
 import com.hansung.hansungcommunity.repository.RecruitBoardRepository;
 import com.hansung.hansungcommunity.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +34,32 @@ public class RecruitBoardService {
         RecruitBoard saved = recruitBoardRepository.save(RecruitBoard.createBoard(dto, user));
 
         return saved.getId();
+    }
+
+    public List<RecruitBoardListDto> getList(Pageable pageable) {
+        Pageable setPage = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.Direction.DESC, "createdAt");
+        return recruitBoardRepository.findAll(setPage).getContent()
+                .stream()
+                .map(RecruitBoardListDto::from)
+                .collect(Collectors.toList());
+    }
+
+    public RecruitBoardDetailDto getDetail(Long boardId) {
+        RecruitBoard recruitBoard = recruitBoardRepository.findById(boardId)
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 구인 게시글이 없습니다."));
+
+        return RecruitBoardDetailDto.from(recruitBoard);
+    }
+
+    /**
+     * 조회수 증가 로직
+     */
+    @Transactional
+    public void increaseHits(Long boardId) {
+        RecruitBoard board = recruitBoardRepository.findById(boardId)
+                .orElseThrow(() -> new IllegalArgumentException("조회수 증가 실패, 해당하는 게시글이 없음"));
+
+        board.increaseHits();
     }
 
 }
