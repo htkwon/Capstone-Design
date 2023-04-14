c8a8d5e3c2cca6cccf3f3978dcb4b53fde7a7d03
htkwon <a3001sd@gmail.com>
2023-04-14 12:50:02 +0900

    FEAT : #113 - Q&A 북마크 로직 추가

---
+++ src/main/java/com/hansung/hansungcommunity/service/QnaBoardBookmarkService.java
@@ -0,0 +1,68 @@
+package com.hansung.hansungcommunity.service;
+
+import com.hansung.hansungcommunity.dto.qna.QnaBoardBookmarkDto;
+import com.hansung.hansungcommunity.dto.qna.QnaBoardRequestDto;
+import com.hansung.hansungcommunity.dto.user.UserBookmarkDto;
+import com.hansung.hansungcommunity.entity.QnaBoard;
+import com.hansung.hansungcommunity.entity.QnaBoardBookmark;
+import com.hansung.hansungcommunity.entity.User;
+import com.hansung.hansungcommunity.repository.QnaBoardBookmarkRepository;
+import com.hansung.hansungcommunity.repository.QnaBoardRepository;
+import com.hansung.hansungcommunity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class QnaBoardBookmarkService {
+
+    private final QnaBoardBookmarkRepository qnaBoardBookmarkRepository;
+    private final UserRepository userRepository;
+    private final QnaBoardRepository qnaBoardRepository;
+
+    public List<QnaBoardBookmarkDto> getBoards(Long userId) {
+        List<QnaBoardBookmark> qnaBoardBookmarks = qnaBoardBookmarkRepository.findAllByUserId(userId)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다."));
+        return qnaBoardBookmarks.stream()
+                .map(bookmark -> {
+                    UserBookmarkDto userBookmarkDto = UserBookmarkDto.of(bookmark.getUser());
+                    QnaBoardRequestDto qnaBoardRequestDto = QnaBoardRequestDto.of(bookmark.getQnaBoard());
+                    return new QnaBoardBookmarkDto(userBookmarkDto,qnaBoardRequestDto);
+                }).collect(Collectors.toList());
+    }
+    @Transactional
+    public QnaBoardBookmark create(Long boardId, Long id) {
+        QnaBoard qnaBoard = qnaBoardRepository.findById(boardId)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다."));
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다."));
+        
+        QnaBoardBookmark qnaBoardBookmark = QnaBoardBookmark.of(user, qnaBoard);
+        return qnaBoardBookmarkRepository.save(qnaBoardBookmark);
+    }
+    @Transactional
+    public void cancle(Long boardId, Long userId) {
+        QnaBoardBookmark qnaBoardBookmark = qnaBoardBookmarkRepository.findByQnaBoardIdAndUserId(boardId,userId)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다."));
+        qnaBoardBookmarkRepository.delete(qnaBoardBookmark);
+    }
+
+    public Boolean check(Long boardId, Long userId) {
+        QnaBoardBookmark qnaBoardBookmark = qnaBoardBookmarkRepository.findByQnaBoardIdAndUserId(boardId,userId)
+                .orElse(null);
+        return qnaBoardBookmark!=null;
+
+    }
+
+    public int count(Long boardId) {
+        List<QnaBoardBookmark> list = qnaBoardBookmarkRepository.findAllByQnaBoardId(boardId)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다."));
+        return list.size();
+    }
+}
